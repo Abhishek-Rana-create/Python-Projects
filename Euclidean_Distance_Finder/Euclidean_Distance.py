dc7001302c55ce1f4230635289af1938f33b1fa8
Abhishek Singh <ranaabhi858@gmail.com>
2026-06-30 02:04:08 +0530

    add input function

--- Euclidean_Distance_Finder/Euclidean_Distance.py
+++ Euclidean_Distance_Finder/Euclidean_Distance.py
@@ -1,25 +1,12 @@
-def main():
-    # ask user for coordinates of two points of First point
-    x1, y1 = float(input("Enter x coordinate of first point: ")), float(input("Enter y coordinate of first point: "))
-
-    # ask user for coordinates of two points of Second point
-    x2, y2 = (float(input("Enter x coordinate of second point: ")), float(input("Enter y coordinate of second point: ")))
-
-    a = (x1, y1)
-    b = (x2, y2)
-
-    return a, b
-a, b = main()
+# Prompt user to enter coordinates.
+# Basic Error-handaling for non-numeric input.
+def get_input(point):
+    while True:
+        try:
+            x = float(input("Enter the x coordinate of {point} point: "))
+            y = float(input("Enter the y coordinate of {point} point: "))
+            return (x, y)
+        except: ValueError
+        print("Invalid Values. Please enter valid numeric values")
 
 
-def value_printer(a, b):
-    print(f"first point coordinates are: ",{a})
-    print(f"second point coordinates are: ",{b})
-value_printer(a, b)
-
-def Euclidean_Distance_operator(a, b):
-    print(f"The distance between these two points is: ", ({b[0]-a[0]}) + ({b[1]-a[1]}), "units, as calculated from second point to first point.")
-Euclidean_Distance_operator(a, b)
-
-
-main()
